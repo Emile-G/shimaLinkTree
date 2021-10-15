e93c23f36045a3f247f565ac7435982908262fa5
Emile-G <egardent.pro@gmail.com>
2021-10-15 22:37:26 +0200

    V0.5 ShimsLinkTree

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'shima': "url('/src/images/shimaWP.png')",
+        'shima-phone': "url('/src/images/shimaWPPhone.png')",
+      },
+    },
   },
   variants: {
     extend: {},
